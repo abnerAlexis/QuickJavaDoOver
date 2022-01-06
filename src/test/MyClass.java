fdd2cb9e233d7ae0669c2856af67b2110e34b2db
Alexis Abner <alexisjuliaabner@gmail.com>
2022-01-05 17:32:58 -0700

    Create returning method.

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -2,11 +2,7 @@ package test;
 
 public class MyClass {
 //if i is static you can not declare a value for i in constructor. It has to be in same line.
-    final int i;
-
-    public MyClass() {
-        i = 5;
-    }
+    static int i = 5;
 
     public static void main(String[] args) {
         MyClass.initialize();
@@ -14,10 +10,16 @@ public class MyClass {
         System.out.println(myClass.i);
         System.out.println(MyConstants.LONG_WAIT);
         System.out.println(EnumConstants.SUNDAY);
+        System.out.println(returningMethod());
     }
 
     public static void initialize() {
         System.out.println("I'm in initialize method.");
+    }
+
+    public static int returningMethod(){
+        i = 6;
+        return i;
     }
 }
 
