1bd66a489c8fac16bc17d3a42e98caf34dc4b5a1
Alexis Abner <alexisjuliaabner@gmail.com>
2022-01-05 20:39:44 -0700

    Create String variable.

--- src/test/MyClass.java
+++ src/test/MyClass.java
@@ -3,6 +3,9 @@ package test;
 public class MyClass {
 //if i is static you can not declare a value for i in constructor. It has to be in same line.
     static int i = 5;
+    String str = "Hello World";
+
+    public MyClass() {}
 
     public static void main(String[] args) {
         MyClass.initialize();
@@ -11,6 +14,8 @@ public class MyClass {
         System.out.println(MyConstants.LONG_WAIT);
         System.out.println(EnumConstants.SUNDAY);
         System.out.println(returningMethod());
+        System.out.println(myClass.creturningMethod().i);
+        System.out.println(myClass.creturningMethod().str);
     }
 
     public static void initialize() {
@@ -21,6 +26,13 @@ public class MyClass {
         i = 6;
         return i;
     }
+
+    public MyClass creturningMethod(){
+        i = 16;
+        return this;
+    }
+
+
 }
 
 /*
